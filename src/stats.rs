4fa0d4488b456c8fe166e96c47efa3a8d21bfae0
jacques <jacques.perrault@branch.technology>
2022-04-05 20:54:07 -0400

    added time variance

--- src/stats.rs
+++ src/stats.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, NaiveTime, Utc};
+use chrono::{DateTime, Duration, NaiveTime, Utc};
 
 #[derive(Clone, Copy)]
 pub struct MessageStats {
@@ -36,6 +36,7 @@ pub struct TopicStats {
     message_count: i64,
     bytes_avg: f64,
     bytes_avg_variance: f32,
+    time_avg_variance: Duration
 }
 
 impl TopicStats {
@@ -48,7 +49,8 @@ impl TopicStats {
             created: time,
             message_count: 1,
             bytes_avg: bytes as f64,
-            bytes_avg_variance: 0.0
+            bytes_avg_variance: 0.0,
+            time_avg_variance: Duration::seconds(0)
         }
     }
 
@@ -59,10 +61,12 @@ impl TopicStats {
 
         let bytes_avg = self.bytes_avg + (bytes as f64 - self.bytes_avg)/message_count as f64;
 
-        let current_variance = (self.last.bytes - bytes).abs();
-        let bytes_avg_variance= self.bytes_avg_variance + (current_variance as f32 - self.bytes_avg_variance)/self.message_count as f32;
+        let current_bytes_difference = (self.last.bytes - bytes).abs();
+        let bytes_avg_variance= self.bytes_avg_variance + (current_bytes_difference as f32 - self.bytes_avg_variance)/self.message_count as f32;
 
-        let current_time_difference = self.last.time - time;
+        let current_time_difference = (self.last.time - time) * -1;
+        let time_avg_variance= self.time_avg_variance + (current_time_difference - self.time_avg_variance)/ self.message_count as i32;
+
         // generate a new topic stats object - should probably be mutating current struct
         TopicStats{
             old: self.last,
@@ -72,13 +76,18 @@ impl TopicStats {
             message_count,
             bytes_avg,
             bytes_avg_variance,
+            time_avg_variance
         }
     }
 }
 
 #[cfg(test)]
 mod stats_tests{
+    use std::thread;
+    use std::time::Duration;
     use crate::stats::TopicStats;
+    use chrono;
+
 
     #[test]
     fn swap(){
@@ -105,5 +114,15 @@ mod stats_tests{
         assert_eq!(tp.bytes_avg_variance, 20.0);
         let tp = tp.create_datapoint(32, 1);
         assert_eq!(tp.bytes_avg_variance, 10.0);
+    }
+    #[test]
+    fn time_variance() {
+        let mut tp = TopicStats::new(12, 2);
+        assert_eq!(tp.time_avg_variance, chrono::Duration::seconds(0));
+
+        thread::sleep(Duration::from_secs(1));
+        let tp = tp.create_datapoint(32, 1);
+        let d = chrono::Duration::seconds(1);
+        assert!(tp.time_avg_variance > d);
     }
 }
