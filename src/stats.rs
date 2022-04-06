463299bd32d30257caec2bdf4a883214dd7ab545
jacques <jacques.perrault@branch.technology>
2022-04-05 20:38:16 -0400

    added chrono

--- src/stats.rs
+++ src/stats.rs
@@ -1,20 +1,20 @@
-use std::time::SystemTime;
+use chrono::{DateTime, NaiveTime, Utc};
 
 #[derive(Clone, Copy)]
 pub struct MessageStats {
     bytes: i32,
-    time:SystemTime
+    time:NaiveTime
 }
 
 impl MessageStats {
     pub fn new()->Self{
         MessageStats{
             bytes: 0,
-            time: SystemTime::now(),
+            time: Utc::now().time(),
         }
     }
 
-    pub fn from_time(time: SystemTime) -> Self {
+    pub fn from_time(time: NaiveTime) -> Self {
        MessageStats{
            time,
            bytes: 0
@@ -32,7 +32,7 @@ pub struct TopicStats {
     last: MessageStats,
     // meta stats
     qos: i32,
-    created: SystemTime,
+    created: NaiveTime,
     message_count: i64,
     bytes_avg: f64,
     bytes_avg_variance: f32,
@@ -40,7 +40,7 @@ pub struct TopicStats {
 
 impl TopicStats {
     pub fn new(bytes: i32, qos: i32)->Self{
-        let time = SystemTime::now();
+        let time = Utc::now().time();
         TopicStats{
             old: MessageStats::from_time(time),
             last: MessageStats{bytes, time},
@@ -55,16 +55,23 @@ impl TopicStats {
     pub fn create_datapoint(&self, bytes: i32, qos: i32) -> Self{
 
         let message_count = self.message_count + 1;
+        let time = Utc::now().time();
+
+        let bytes_avg = self.bytes_avg + (bytes as f64 - self.bytes_avg)/message_count as f64;
+
         let current_variance = (self.last.bytes - bytes).abs();
+        let bytes_avg_variance= self.bytes_avg_variance + (current_variance as f32 - self.bytes_avg_variance)/self.message_count as f32;
+
+        let current_time_difference = self.last.time - time;
         // generate a new topic stats object - should probably be mutating current struct
         TopicStats{
             old: self.last,
-            last: MessageStats{bytes, time: SystemTime::now()},
+            last: MessageStats{bytes, time: Utc::now().time()},
             qos,
             created: self.created,
             message_count,
-            bytes_avg: self.bytes_avg + (bytes as f64 - self.bytes_avg)/message_count as f64,
-            bytes_avg_variance: self.bytes_avg_variance + (current_variance as f32 - self.bytes_avg_variance)/self.message_count as f32,
+            bytes_avg,
+            bytes_avg_variance,
         }
     }
 }
@@ -95,8 +102,8 @@ mod stats_tests{
     fn bytes_variance() {
         let mut tp = TopicStats::new(12, 2);
         let tp = tp.create_datapoint(32, 1);
-        assert_eq!(tp.bytes_avg, 20.0);
+        assert_eq!(tp.bytes_avg_variance, 20.0);
         let tp = tp.create_datapoint(32, 1);
-        assert_eq!(tp.bytes_avg, 10.0);
+        assert_eq!(tp.bytes_avg_variance, 10.0);
     }
 }
